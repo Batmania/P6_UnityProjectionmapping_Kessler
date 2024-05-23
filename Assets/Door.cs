0394a04e3db7d73a7fac31971fb71c642db4d81e
Batmania <ikessler116@gmail.com>
2024-05-23 18:49:31 +0200

    Added Animation and fixed door

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     // Start is called before the first frame update
-    public bool doorIsOpen = false;
-    public Transform door; 
+    public bool isOpen = false;
+    public Transform rotationPoint;
+    public BoxCollider collisionBox;
+    public float openingSpeed = 90F;
+
+    private float maxYRotation = 90.0F;
+    private float currentYRotationAlpha = 0F;
+
     void Start()
     {
-        
+        if (isOpen)
+        {
+            rotationPoint.localRotation = Quaternion.Euler(x:0,y:maxYRotation,z:0);
+            currentYRotationAlpha = 1F;
+        }
+        else
+        {
+            rotationPoint.localRotation = Quaternion.Euler(x:0,y:0,z:0);
+        }   
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (doorIsOpen)
-        {
-            door.rotation = Quaternion.Euler(x:0,y:90,z:0);
-        }
-        else
-        {
-            door.rotation = Quaternion.Euler(x:0,y:0,z:0);
-        }
+        float newYRotationAlpha = currentYRotationAlpha + ((Time.deltaTime * openingSpeed * (isOpen ? 1F : -1F)) / maxYRotation);
+        currentYRotationAlpha = Mathf.Clamp(newYRotationAlpha, 0F, 1F);
+
+        rotationPoint.localRotation = Quaternion.Euler(x: 0, y: /*lerpResult*/currentYRotationAlpha * maxYRotation, z: 0);
     }
 }
